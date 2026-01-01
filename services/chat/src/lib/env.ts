780065a13ada0455e358c3796e8469210c3b6f17
hnm <mistrhnm@gmail.com>
2026-01-01 23:21:01 +0530

    fix: resolve conflict

--- services/chat/src/lib/env.ts
+++ services/chat/src/lib/env.ts
@@ -14,11 +14,13 @@ export const env = createEnv({
       .enum(["development", "staging", "production"])
       .default("development"),
     PORT: z.coerce.number().min(3000).max(9999),
-
     /* Logger */
     LOGGER_LEVEL: z
       .enum(["trace", "debug", "info", "warn", "error", "fatal"])
       .default("info"),
     LOGGER_TARGET: z.enum(["pino/file", "pino-pretty"]).default("pino-pretty"),
+
+    /* DB  */
+    DATABASE_URL: z.url(),
   },
 });
